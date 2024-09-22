e9bcc92d4076d50e05feceb406b359e3dc40c254
wahyu tricahyo mulyo <wahyu.tricahyo@loyalto.id>
2024-09-22 15:39:42 +0700

    update next configt

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
